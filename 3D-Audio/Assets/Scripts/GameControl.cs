f98a37e0798a77fc9ca35bb7c076d6c37d036321
r-clarke <62532387+r-clarke@users.noreply.github.com>
2021-04-14 23:19:11 -0400

    Scoring, randomization, reaction speed averaging complete

--- 3D-Audio/Assets/Scripts/GameControl.cs
+++ 3D-Audio/Assets/Scripts/GameControl.cs
@@ -8,21 +8,32 @@ public class GameControl : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private Text gameText;
-    
+
+    [SerializeField]
+    private Text scoreText;
+
+    [SerializeField]
+    private Text statusText;
 
     public AudioClip[] testingSounds;
     public AudioSource m_audioSource;
 
-    private float reactionTime, startTime;
+    private float reactionTime, startTime, totalTime;
     private bool clockIsTicking, timerCanBeStopped;
-    private int selected;
+    private int counter, score;
+    public int selected;
+
     void Start()
     {
         m_audioSource = GetComponent<AudioSource>();
-
+        counter = 5;
+        score = 0;
         reactionTime = 0f;
         startTime = 0f;
+        totalTime = 0f;
         gameText.text = "---";
+        scoreText.text = "---";
+        statusText.text = "Left Click to Begin!";
         clockIsTicking = false;
         timerCanBeStopped = true;
     }
@@ -30,44 +41,226 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.anyKeyDown)
         {
-            if (!clockIsTicking)
-            {
-                StartCoroutine("StartMeasuring");
-                gameText.text = "Wait";
-                //indicate waiting for reaction (play sound)
-                clockIsTicking = true;
-                timerCanBeStopped = false;
-            } 
+            if (counter > 0) 
+            { 
+                if (Input.GetMouseButtonDown(0))
+                {
+                    StartCoroutine("StartMeasuring");
+                    gameText.text = "Wait";
+                    scoreText.text = score.ToString();
+                    clockIsTicking = true;
+                    timerCanBeStopped = false;
+                }
+                else
+                {
+                    if (Input.GetKeyDown("[1]") && selected == 0)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+                    
+
+                    }
+                    else if (Input.GetKeyDown("[2]") && selected == 1)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[3]") && selected == 2)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[4]") && selected == 3)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[6]") && selected == 4)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[7]") && selected == 5)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[8]") && selected == 6)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else if (Input.GetKeyDown("[9]") && selected == 7)
+                    {
+                        if (clockIsTicking && timerCanBeStopped)
+                        {
+                            StopCoroutine("StartMeasuring");
+                            reactionTime = Time.time - startTime;
+                            totalTime += reactionTime;
+                            gameText.text = reactionTime.ToString("N3");
+                            score += 1;
+                            scoreText.text = score.ToString();
+                            counter -= 1;
+                            clockIsTicking = false;
+                        }
+
+                        if(counter != 0)
+                        {
+                                StartCoroutine("StartMeasuring");
+                                scoreText.text = score.ToString();
+                                clockIsTicking = true;
+                                timerCanBeStopped = false;
+                        }
+
+                    }
+                    else
+                    {
+                        //play wrong sound
+                    }
+                }
+            }
+            
         }
-        if (Input.GetKeyDown("[4]"))
+        if(counter == 0)
         {
-            if (clockIsTicking && timerCanBeStopped)
-            {
-                StopCoroutine("StartMeasuring");
-                reactionTime = Time.time - startTime;
-                gameText.text = reactionTime.ToString("N3");
-                clockIsTicking = false;
-            }
-            else if (clockIsTicking && !timerCanBeStopped)
-            {
-                StopCoroutine("StartMeasuring");
-                reactionTime = 0f;
-                clockIsTicking = false;
-                timerCanBeStopped = true;
-                gameText.text = "Too early";
-            }
+            statusText.text = "Final Score: " + score.ToString() + "\nAverage Speed: " + (totalTime / score).ToString("N3") + "s";
         }
     }
 
     private IEnumerator StartMeasuring()
     {
-        //randomDelayBeforeMeasuring = Random.Range(0.5f, 5f);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(1f);
         //indicate that reaction must be made (play sound now)
         selected = Random.Range(0, 8);
-        Debug.Log(selected);
+        //Debug.Log(selected);
         m_audioSource.PlayOneShot(testingSounds[selected]);
 
         startTime = Time.time;
